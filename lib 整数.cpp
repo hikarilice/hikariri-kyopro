c9053f60a1e8aaa20f8a2c0b844c5c95dc1405c1
hikarilice <130908474+hikarilice@users.noreply.github.com>
2023-10-08 19:11:19 +0900

    Update lib 整数.cpp

--- lib 整数.cpp
+++ lib 整数.cpp
@@ -1,22 +1,3 @@
-//#include <bits/stdc++.h>
-#include "stdc++.hの中身.cpp"
-
-//#include <bits/stdc++.h>
-using namespace std;
-#if __has_include(<atcoder/all>)
-#include <atcoder/all>
-using namespace atcoder;
-typedef modint998244353 mint;
-#endif
-typedef long long  ll;
-typedef pair<int,int> pii; typedef pair<ll,ll> pll;
-const int INF = 1e9; const long long LINF = 1e18;
-const int MOD = 998244353; const int MOD1 = 1e9+7;
-#define rep(i,n) for(int i=0;i<(int)(n);i++)
-#define all(x) x.begin(), x.end()
-template<typename T> bool chmin(T& a, T b){if(a>b){a=b; return true;} return false;}
-template<typename T> bool chmax(T& a, T b){if(a<b){a=b; return true;} return false;}
-void yorn(bool ans){cout << (ans?"Yes":"No") << endl; return;}
 
 // a^b%m (巨大数の累乗のmod)
 ll modpow(ll a, ll b, ll m){
