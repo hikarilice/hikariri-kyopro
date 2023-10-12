bf27af043a9a1381efd0e0e9ad3004f8bd2979d7
hikarilice <130908474+hikarilice@users.noreply.github.com>
2023-10-12 18:40:15 +0900

    Update BFS.cpp

--- BFS.cpp
+++ BFS.cpp
@@ -18,16 +18,16 @@ void BFS (vvl &g, queue<ll> &q, vl &d, ll start){
 // ラムダ式版
 // g,q,dを定義する必要あり
 auto BFS = [&](vvl &g, queue<ll> &q, vl &d, ll start){
-        q.push(start);
-        d.at(start) = 0;
-        while(!q.empty()){
-            ll pos = q.front();
-            q.pop();
-            for(auto i:g.at(pos)){
-                if(d.at(i)!=-1) continue;
-                q.push(i);
-                d.at(i) = d.at(pos)+1;
-            }
+    q.push(start);
+    d.at(start) = 0;
+    while(!q.empty()){
+        ll pos = q.front();
+        q.pop();
+        for(auto i:g.at(pos)){
+        if(d.at(i)!=-1) continue;
+            q.push(i);
+            d.at(i) = d.at(pos)+1;
         }
-        return;
-    };
+    }
+    return;
+};
